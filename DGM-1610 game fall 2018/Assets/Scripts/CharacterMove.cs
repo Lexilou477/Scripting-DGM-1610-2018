4f048c68bd318cc6b4e73561fae9cd3f822db421
Unknown <alexis.hardman47@gmail.com>
2018-09-12 17:17:03 -0600

    Added to CharacterMove script

--- DGM-1610 game fall 2018/Assets/Scripts/CharacterMove.cs
+++ DGM-1610 game fall 2018/Assets/Scripts/CharacterMove.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
+	// Player Movement Variables
 	public int MoveSpeed; 
 	public float JumpHeight;
 
+	// Player grounded variables
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool Grounded;
+
 	// Use this for initialization
 	void Start () {
-		print("Hello World!");
+		
 	}
 	
+
+	void FixedUpdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
+	}
+		
 	// Update is called once per frame
 	void Update () {
-		
+
+		//This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+			Jump();
+		}
+	
+	}
+
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
