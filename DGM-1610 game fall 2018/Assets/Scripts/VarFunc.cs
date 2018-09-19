f29201e21542090673d5e4f7fe59864ba89b84e7
Unknown <alexis.hardman47@gmail.com>
2018-09-19 17:16:36 -0600

    Edited groundcheck so we can actually jump and move

---
+++ DGM-1610 game fall 2018/Assets/Scripts/VarFunc.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VarFunc : MonoBehaviour {
+
+	public bool PlaceHolder;
+
+	private int Test = 5;
+
+	void Once () {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
