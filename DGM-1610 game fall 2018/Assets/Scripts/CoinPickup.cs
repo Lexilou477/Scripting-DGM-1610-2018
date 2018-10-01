80c5a9a1534da7ce2cea03176c142a8870616bca
Unknown <alexis.hardman47@gmail.com>
2018-10-01 17:13:59 -0600

    Added SpriteSheet created CoinPickup script and added coin

---
+++ DGM-1610 game fall 2018/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+	public int PointsToAdd;
+
+	void OnTriggerEnter2D (Collider2D Other){
+		if (Other.GetComponent<Rigidbody2D> () == null)
+			return;
+
+		ScoreManager.AddPoints (PointsToAdd);
+
+		Destroy (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
