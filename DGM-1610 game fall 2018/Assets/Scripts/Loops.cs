abd956ef73fedf6ec76f709f242ea85d4848c051
Unknown <alexis.hardman47@gmail.com>
2018-10-24 17:17:05 -0600

    Added Loops and Coin sprite

---
+++ DGM-1610 game fall 2018/Assets/Scripts/Loops.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	public int Num = 40;
+	public string Color = "Blue";
+
+	// Use this for initialization
+	//(int starting point(0 counting up); range(how long)(< counting up); incrimenter(++ counting up))
+	void Start () {
+		//  for(int i=0; i <= 100; i++){
+		//  	print(i + "Ants Marching");
+		//  }
+
+		//  while(Num >= 0){
+		// 	print("Countdown" + Num);
+		//  	Num --;
+		//  }
+
+		// while(Color == "Red"){
+		// 	print("Color is " + Color);
+		// }
+
+		for(int Var=10; Var > 0; Var--){
+			print("Launch in " + Var);
+		}
+
+		for(int a=0; a < 25; a++){
+			print("Apples in Basket " + a);
+		}
+
+		for(int C=50; C > 1; C--){
+			print("Cookies in Jar " + C);
+		}
+
+	// 3 more for and 3 more while
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
