4ffd2ec2043faec3855acb51cafeb4bd0c90e2fd
Unknown <alexis.hardman47@gmail.com>
2018-10-29 17:16:04 -0600

    Added Homework folder and moved scripts created switches script

--- DGM-1610 game fall 2018/Assets/HomeWork/Loops.cs
+++ DGM-1610 game fall 2018/Assets/HomeWork/Loops.cs
@@ -6,6 +6,8 @@ public class Loops : MonoBehaviour {
 
 	public int Num = 40;
 	public string Color = "Blue";
+	public int Var = 1000;
+	public int One = 0;
 
 	// Use this for initialization
 	//(int starting point(0 counting up); range(how long)(< counting up); incrimenter(++ counting up))
@@ -23,17 +25,27 @@ public class Loops : MonoBehaviour {
 		// 	print("Color is " + Color);
 		// }
 
-		for(int Var=10; Var > 0; Var--){
-			print("Launch in " + Var);
-		}
+		// for(int Var=10; Var > 0; Var--){
+		// 	print("Launch in " + Var);
+		// }
 
-		for(int a=0; a < 25; a++){
-			print("Apples in Basket " + a);
-		}
+		// for(int a=0; a < 25; a++){
+		// 	print("Apples in Basket " + a);
+		// }
 
-		for(int C=50; C > 1; C--){
-			print("Cookies in Jar " + C);
-		}
+		// for(int C=50; C > 1; C--){
+		// 	print("Cookies in Jar " + C);
+		// }
+
+		// while(Var > 1){
+		// 	print("Leaves on tree " + Var);
+		// 		Var --;
+		// }
+
+		// while(One < 100){
+		// 	print("Cars on road " + One);
+		// 	One ++;
+		// }
 
 	// 3 more for and 3 more while
 	}
---
+++ DGM-1610 game fall 2018/Assets/HomeWork/Switches.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour {
+
+	public int Num;
+	public string Name;
+	public string Fruit;
+	public float Loto;
+	public bool Light;
+
+	// Use this for initialization
+	
+	void Start () {
+		// if(Num == 1){
+		// 	print("Hurray, you picked one!");
+		// }
+		// else if(Num == 2){
+		// 	print("Hurray, you picked two!");
+		// }
+		// else{
+		// 	print("Boo, you didn't pick one!");
+		// }
+	// Num picks the case number, so if you make Num 1, you will print case 1
+	// Case designates Variable(Num)^	
+	//default is a catch all, whatever doesn't have a case
+		// switch(Num){
+		// 	case 4:
+		// 		print("You picked " + Num);
+		// 	break;
+
+		// 	case 3:
+		// 		print("You picked " + Num);
+		// 	break;
+
+		// 	case 6:
+		// 		print("You picked " + Num);
+		// 	break;
+
+		// 	case 10:
+		// 		print("You picked " + Num);
+		// 	break;
+
+		// 	default:
+		// 		print("I don't understand " + Num);
+		// 	break;
+		// }
+
+		switch(Name){
+			case "Jason":
+				print("Welcome to camp Crystal Lake " + Name);
+			break;
+
+			case "Michael":
+				print("Welcome to Haddonfield, Illonois " + Name);
+			break;
+
+			case "Freddie":
+				print("Welcome to Elm Street " + Name);
+			break;
+
+			case "LeatherFace":
+				print("The stars and stripes are big at night...Deep in the heart of Texas");
+			break;
+
+			default:
+				print("I don't know " + Name);
+			break;
+
+		}
+		
+		// switch(Fruit){
+		// 	case "Apple":
+		// 		print("Good choice, you picked " + Fruit);
+		// 	break;
+		// 	case "Orange":
+		// 		print("Good choice, you picked " + Fruit);
+		// 	break;
+		// 	case "Banana":
+		// 		print("Good choice, you picked " + Fruit);
+		// 	break;
+		// 	default:
+		// 		print("We don't have anymore " + Fruit);
+		// 	break;
+		// }
+
+		// switch(Loto){
+		// 	case 4.5:
+		// 		print("You Win! The lucky number was " + Loto);
+		// 	break;
+		// 	case 7:
+		// 		print("You Win! The lucky number was " + Loto);
+		// 	break;
+		// 	case 1000:
+		// 		print("You Win! The lucky number was " + Loto);
+		// 	break;
+		// 	default:
+		// 		print("Sorry, try again :(");
+		// 	break;
+		// }
+
+		switch(Light){
+			case true:
+				print("The light is on!");
+			break;
+			case false:
+				print("The light is off!");
+			break;
+		}
+	}
+	
+	
+}
