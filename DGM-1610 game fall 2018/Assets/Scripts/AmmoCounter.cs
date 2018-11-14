2e184ce70a3bdacf581c9afe930fcd45327c7f1d
Unknown <alexis.hardman47@gmail.com>
2018-11-14 16:56:41 -0700

    Destroyed particles added coin ignore layers and Pickup scripts

---
+++ DGM-1610 game fall 2018/Assets/Scripts/AmmoCounter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoCounter : MonoBehaviour {
+
+	public int Ammo;
+	public int MaxAmmo;
+	Text AmmoNumber;
+
+	// Use this for initialization
+	void Start () {
+		AmmoNumber = GetComponent<Text>();
+
+		Ammo = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Ammo < 0)
+		Ammo = 0;
+		
+		AmmoNumber.text = " " + Ammo;
+		
+	}
+
+	public static void AddAmmo (int AmmoToAdd){
+		Ammo += AmmoToAdd;
+	}
+}
